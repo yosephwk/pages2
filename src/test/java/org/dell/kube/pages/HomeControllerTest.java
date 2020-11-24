8bd8d331cef8627a719e11019569d4f32e0c6aac
Nirav Yosep <myinboxaus@gmail.com>
2020-11-24 12:46:22 +0000

    chapter 1 completed

---
+++ src/test/java/org/dell/kube/pages/HomeControllerTest.java
@@ -0,0 +1,17 @@
+package org.dell.kube.pages;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HomeControllerTest {
+    private final String message = "YellowPages";
+
+    @Test
+    public void itSaysYellowPagesHello() throws Exception {
+        HomeController controller = new HomeController();
+
+        assertThat(controller.getPage()).contains(message);
+    }
+
+
+}
